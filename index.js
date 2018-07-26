390c1ad951fd3f58e80eca11d85afc4523885e14
Dries Hooghe <dries.hooghe@gmail.com>
2018-07-26 21:50:19 +0200

    Add row to sheet and send post to slack

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
 // Requirements
+var GoogleSpreadsheet = require('google-spreadsheet');
+var moment = require('moment-timezone');
 
+var creds = require('./credentials.json');
+var sheetInfo = require('./sheet_info.json');
+var slackInfo = require('./slack_info.json');
 
 
 // Variables
-
-
-
-
-// Helper functions
-
-
-
+var doc = new GoogleSpreadsheet(sheetInfo.id);
+const { IncomingWebhook } = require('@slack/client');
+const webhook = new IncomingWebhook(slackInfo.url);
 
 // Entrypoint
 exports.log = (req, res) => {
-  console.log('Logging timestamp to sheet');
+
+  var now = moment().tz('Europe/Brussels');
+  var time = '=TIME(' + now.format('HH, mm, ss') + ')';
+  var date = '=DATE(' + now.format('YYYY, MM, DD') + ')';
+  var message = 'Dries badged at ' + now.format('HH:mm on MMMM Do YYYY');
+
+  var result = doc.useServiceAccountAuth(creds, function (err) {
+
+    doc.addRow(sheetInfo.tabId, {
+      "DATE": date,
+      "TIME": time
+    }, function (err) {
+      if (err) {
+        message = 'Whoops, looks like something went wrong when you badged:' + err;
+      }
+    }
+    );
+  });
+
+  webhook.send(message, function (err, res) {
+    if (err) {
+      console.log('Error:', err);
+    } else {
+      console.log('Message sent: ', res);
+    }
+  });
 };
